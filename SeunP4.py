9f3bd3e9341527a9fbe274bc0b042dd1fae7424a
Seun <77089905+asajayi21@users.noreply.github.com>
2023-06-15 00:27:00 -0600

    Add files via upload

---
+++ SeunP4.py
@@ -0,0 +1,19 @@
+#SeunP4.py
+#Programmer: A. Seun Ajayi
+#Email: aajayi@cnm.edu
+#Purpose: provides user capability to translate a phrase from Spanish to English
+
+#Create the dictionary
+commonPhrase = {"Hola":"Hello","Como estas?":"How are you?","Bien":"Good","Gracias":"Thank you"}
+
+#Print out the keys
+print(commonPhrase.keys())
+
+#Ask for input from user
+word = input("Please enter a phrase to translate: ")
+
+#Checks phrase is in dictionary or prints not able to translate
+if word in commonPhrase: 
+    print("Your sentence in english: {}".format(commonPhrase.get(word,"n/a")))
+else:
+    print("Unable to translate.")
